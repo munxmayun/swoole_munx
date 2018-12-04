94e9c7dd73a640c59f53377a49f88fb367ea7141
munxmayun@163.com <317083802@qq.com>
2018-12-04 23:52:56 +0800

    munx

--- demo/server/tcp.php
+++ demo/server/tcp.php
@@ -3,14 +3,22 @@
 //创建Server对象，监听 127.0.0.1:9501端口
 $serv = new swoole_server("127.0.0.1", 9501);
 
+$serv->set([
+    'worker_num' => 4, //进程数 cpu 1-4
+    'max_request' => 10000,
+]);
 //监听连接进入事件
-$serv->on('connect', function ($serv, $fd) {
-    echo "Client: Connect.\n";
+/**
+ * $fd 客户端连接的唯一标识
+ * $reactor_id 线程id
+ */
+$serv->on('connect', function ($serv, $fd, $reactor_id) {
+    echo "Client: {$reactor_id}-{$fd}-Connect.\n";
 });
 
 //监听数据接收事件
 $serv->on('receive', function ($serv, $fd, $from_id, $data) {
-    $serv->send($fd, "Server: " . $data);
+    $serv->send($fd, "Server:{$reactor_id}-{$fd} " . $data);
 });
 
 //监听连接关闭事件
